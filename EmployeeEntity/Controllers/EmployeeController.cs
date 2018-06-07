6b46a3939ec8ed9227f1b68ad08f0bc9a7f1b90e
Anjani18 <40016860+Anjani18@users.noreply.github.com>
2018-06-07 15:50:07 +0530

    Add files via upload

---
+++ EmployeeEntity/Controllers/EmployeeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeEntity.Data;
+using EmployeeEntity.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeEntity.Controllers
+{
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var _context = new EmployeeContext())
+            {
+                var listofEmployees = new List<Employee>();
+                listofEmployees = _context.tblEmployee.ToList();
+                return Ok(listofEmployees);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Employee EmployeeObject)
+        {
+            using (var _Context = new EmployeeContext())
+            {
+                _Context.tblEmployee.Add(EmployeeObject);
+                _Context.SaveChanges();
+            }
+            return Ok("EmployeeAdded");
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody]Employee EmployeeObject)
+        {
+            using (var _Context = new EmployeeContext())
+            {
+                _Context.tblEmployee.Update(EmployeeObject);
+                _Context.SaveChanges();
+            }
+            return Ok("employee data has been updated");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody]Employee EmployeeObject)
+        {
+            using (var _Context = new EmployeeContext())
+            {
+                _Context.tblEmployee.Remove(EmployeeObject);
+                _Context.SaveChanges();
+            }
+            return Ok("employee data Deleted");
+        }
+
+    }
+}
---
+++ EmployeeEntity/Data/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeEntity.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeEntity.Data
+{
+
+    public class EmployeeContext:DbContext
+    {
+        public DbSet<Employee> tblEmployee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ABURRA01;Database=Employee;Trusted_Connection=True;MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        
+    }
+}
